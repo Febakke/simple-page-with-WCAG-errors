9cf3ff057b4fe6eca338870e8278686b9e9a005b
Lasse Febakke Straum <33222679+Febakke@users.noreply.github.com>
2026-02-04 06:29:03 +0100

    Simple page up and running

---
+++ web/theme/colors.d.ts
@@ -0,0 +1,17 @@
+/* This file is deprecated and will be removed in a future release. Use types.d.ts instead */
+/* build: v1.11.0 */
+import type {} from '@digdir/designsystemet-types';
+
+// Augment types based on theme
+declare module '@digdir/designsystemet-types' {
+  export interface ColorDefinitions {
+    'main-color-2': never;
+    neutral: never;
+  }
+  export interface SeverityColorDefinitions {
+    info: never;
+    success: never;
+    warning: never;
+    danger: never;
+  }
+}
---
+++ web/theme/types.d.ts
@@ -0,0 +1,16 @@
+/* build: v1.11.0 */
+import type {} from '@digdir/designsystemet-types';
+
+// Augment types based on theme
+declare module '@digdir/designsystemet-types' {
+  export interface ColorDefinitions {
+    'main-color-2': never;
+    neutral: never;
+  }
+  export interface SeverityColorDefinitions {
+    info: never;
+    success: never;
+    warning: never;
+    danger: never;
+  }
+}
