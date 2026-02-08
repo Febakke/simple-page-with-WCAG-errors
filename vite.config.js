f2aa74009b3d6dae4edeb63935fa958ff0182ebc
Lasse Febakke Straum <33222679+Febakke@users.noreply.github.com>
2026-02-08 18:30:34 +0100

    Updated errors

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  root: 'web',
+  base: '/simple-page-with-WCAG-errors/'
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+})
