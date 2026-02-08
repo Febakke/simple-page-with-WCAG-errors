34fcd09017662e7b3915836ff808a9e76e8a9e57
Lasse Febakke Straum <33222679+Febakke@users.noreply.github.com>
2026-02-08 21:17:04 +0100

    Trying to fix not all pages being built

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
-import { defineConfig } from 'vite'
+import { resolve } from "path";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: 'web',
   base: '/simple-page-with-WCAG-errors/',
   build: {
+    target: "esnext",
     outDir: '../dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+        omoss: resolve(__dirname, "omoss.html")
+      },
+      output: {
+        manualChunks: {}, 
+      },
+    },
   },
 })
